bf506256a535eb6db8e0f18cd88502bfcefc4f19
Jay <ja34luv@gmail.com>
2023-11-26 23:33:38 -0800

    update type bar ui

--- src/pages/MessagesPage/Main/Main.js
+++ src/pages/MessagesPage/Main/Main.js
@@ -1,10 +1,16 @@
 import "./Main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import Conversation from "components/Conversation/Conversation";
 import Message from "components/Message/Message";
 import ChatOnline from "components/ChatOnline/ChatOnline";
 
 function Main() {
+    const [message, setMessage] = useState("");
+
+    const isTyping = () => message !== "";
+    console.log(message.length, isTyping());
+
     return (
         <div className="messenger p-0 text-start">
             <div className="chatMenu">
@@ -75,10 +81,32 @@ function Main() {
                                 maxHeight: "140px",
                                 boxSizing: "border-box",
                                 padding: "8px 12px",
+                                position: "relative",
                             }}
                         >
-                            <div className="chatMessageInput" contentEditable>
-                                hi
+                            <div
+                                className="chatMessageInput"
+                                contentEditable
+                                onInput={(e) => {
+                                    const content = e.target.innerText.replace(
+                                        /\n$/,
+                                        ""
+                                    );
+                                    setMessage(content);
+                                }}
+                            />
+                            <div
+                                className="d-flex justify-content-center align-items-center"
+                                style={{
+                                    position: "absolute",
+                                    left: "12px",
+                                    top: "8px",
+                                    lineHeight: "20.078px",
+                                    opacity: isTyping() ? "0" : "1",
+                                    pointerEvents: isTyping() && "none",
+                                }}
+                            >
+                                Aa
                             </div>
                         </div>
                         <FontAwesomeIcon
