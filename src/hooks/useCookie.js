31aaffeecf9cb73a9ab0be5b44dd7fbf57fad6a4
Jay <ja34luv@gmail.com>
2023-12-15 03:40:49 -0800

    debugging

--- src/hooks/useCookie.js
+++ src/hooks/useCookie.js
@@ -1,5 +1,6 @@
 function useCookie(cookieName) {
     const cookies = document.cookie.split(";");
+    console.log(cookies);
 
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i].trim();
